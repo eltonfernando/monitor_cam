ea3c22d448a0508e98890c393b1646288f4e6ee7
eltonfernando <eltonfernando90@gmail.com>
2022-02-01 10:03:48 -0400

    add action

--- monitor/controller.py
+++ monitor/controller.py
@@ -1,8 +1,10 @@
 from PySide6.QtWidgets import QApplication, QMainWindow, QLabel
 from PySide6.QtCore import Slot
+import os
+from glob import glob
 from .view import Ui_MainWindow
 from .setup import __version__
-
+from .data_base import DataBase
 
 class MyLabel(QLabel):
     def __init__(self,parent=None) -> None:
@@ -15,20 +17,53 @@ class Controller(QMainWindow):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.ui.comboBox_cam.currentIndexChanged.connect(self.on_comboBox_cam_setCurrentIndex)
         self.setWindowTitle(f"Monitor {__version__}")
+        self.restore_data()
         
     
-    def add_cam(self,name_cam):
+    def add_cam_view(self,name_cam):
         self.label = MyLabel()
         self.label.set_label(name_cam)
+         
         self.ui.verticalLayout.addWidget(self.label)
         self.ui.comboBox_cam.addItem(name_cam)
         
     @Slot()
     def on_pushButton_add_clicked(self):
         name_cam = self.ui.comboBox_cam.currentText()
+        rstp_link = self.ui.lineEdit_rstp.text()
+        data = DataBase(name_cam)
+        data.set_rstp_link(rstp_link)
+
         print(name_cam)
-        self.add_cam(name_cam)
+        self.add_cam_view(name_cam)
+    
+    @Slot()
+    def on_pushButton_del_clicked(self):
+        index_selected = self.ui.comboBox_cam.currentIndex()
+        name_cam = self.ui.comboBox_cam.currentText()
+        path_file_config = os.path.join("config",f"{name_cam}.json")
+        if os.path.isfile(path_file_config):
+            os.remove(path_file_config)
+            self.ui.comboBox_cam.removeItem(index_selected)
+    
+    @Slot()
+    def on_comboBox_cam_setCurrentIndex(self, index):
+        name_cam = self.ui.comboBox_cam.currentText()
+        data = DataBase(name_cam)
+        self.ui.lineEdit_rstp.setText(str(data.get_rstp_link()))
+
+        print("test inde combo ",name_cam)
+    
+    def restore_data(self):
+        if os.path.isdir("config"):
+            list_config = glob(os.path.join("config","*.json"))
+            print(list_config)
+            if len(list_config)>0:
+                for path_file in list_config:
+                    name_file = os.path.basename(path_file).split(".")[0]
+                    self.add_cam_view(name_file)
 
 
 if __name__ == "__main__":
--- monitor/view/from_ui.py
+++ monitor/view/from_ui.py
@@ -16,8 +16,9 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QComboBox, QGridLayout, QLabel,
-    QMainWindow, QMenuBar, QPushButton, QScrollArea,
-    QSizePolicy, QStatusBar, QVBoxLayout, QWidget)
+    QLineEdit, QMainWindow, QMenuBar, QPushButton,
+    QScrollArea, QSizePolicy, QStatusBar, QVBoxLayout,
+    QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -28,48 +29,60 @@ class Ui_MainWindow(object):
         self.centralwidget.setObjectName(u"centralwidget")
         self.gridLayout_2 = QGridLayout(self.centralwidget)
         self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.gridLayout_3 = QGridLayout()
+        self.gridLayout_3.setObjectName(u"gridLayout_3")
+        self.gridLayout_3.setContentsMargins(5, 5, 5, 5)
+        self.lineEdit_rstp = QLineEdit(self.centralwidget)
+        self.lineEdit_rstp.setObjectName(u"lineEdit_rstp")
+
+        self.gridLayout_3.addWidget(self.lineEdit_rstp, 1, 1, 1, 1)
+
+        self.label_3 = QLabel(self.centralwidget)
+        self.label_3.setObjectName(u"label_3")
+
+        self.gridLayout_3.addWidget(self.label_3, 0, 0, 1, 1)
+
+        self.label_4 = QLabel(self.centralwidget)
+        self.label_4.setObjectName(u"label_4")
+
+        self.gridLayout_3.addWidget(self.label_4, 1, 0, 1, 1)
+
         self.pushButton_add = QPushButton(self.centralwidget)
         self.pushButton_add.setObjectName(u"pushButton_add")
 
-        self.gridLayout_2.addWidget(self.pushButton_add, 2, 0, 1, 1)
+        self.gridLayout_3.addWidget(self.pushButton_add, 2, 1, 1, 1)
 
-        self.verticalLayout_2 = QVBoxLayout()
-        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.comboBox_cam = QComboBox(self.centralwidget)
+        self.comboBox_cam.setObjectName(u"comboBox_cam")
+        self.comboBox_cam.setEditable(True)
 
-        self.gridLayout_2.addLayout(self.verticalLayout_2, 0, 0, 1, 1)
+        self.gridLayout_3.addWidget(self.comboBox_cam, 0, 1, 1, 1)
+
+        self.pushButton_del = QPushButton(self.centralwidget)
+        self.pushButton_del.setObjectName(u"pushButton_del")
+
+        self.gridLayout_3.addWidget(self.pushButton_del, 3, 1, 1, 1)
+
+
+        self.gridLayout_2.addLayout(self.gridLayout_3, 1, 0, 1, 1)
 
         self.scrollArea = QScrollArea(self.centralwidget)
         self.scrollArea.setObjectName(u"scrollArea")
         self.scrollArea.setWidgetResizable(True)
         self.scrollAreaWidgetContents = QWidget()
         self.scrollAreaWidgetContents.setObjectName(u"scrollAreaWidgetContents")
-        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 490, 345))
+        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 490, 285))
         self.gridLayout = QGridLayout(self.scrollAreaWidgetContents)
         self.gridLayout.setObjectName(u"gridLayout")
         self.verticalLayout = QVBoxLayout()
         self.verticalLayout.setObjectName(u"verticalLayout")
-        self.label_2 = QLabel(self.scrollAreaWidgetContents)
-        self.label_2.setObjectName(u"label_2")
-
-        self.verticalLayout.addWidget(self.label_2)
-
-        self.label = QLabel(self.scrollAreaWidgetContents)
-        self.label.setObjectName(u"label")
-
-        self.verticalLayout.addWidget(self.label)
-
+        self.verticalLayout.setContentsMargins(2, 2, 2, 2)
 
         self.gridLayout.addLayout(self.verticalLayout, 0, 0, 1, 1)
 
         self.scrollArea.setWidget(self.scrollAreaWidgetContents)
 
-        self.gridLayout_2.addWidget(self.scrollArea, 3, 0, 1, 1)
-
-        self.comboBox_cam = QComboBox(self.centralwidget)
-        self.comboBox_cam.setObjectName(u"comboBox_cam")
-        self.comboBox_cam.setEditable(True)
-
-        self.gridLayout_2.addWidget(self.comboBox_cam, 1, 0, 1, 1)
+        self.gridLayout_2.addWidget(self.scrollArea, 10, 0, 1, 1)
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
@@ -87,8 +100,9 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"Nome", None))
+        self.label_4.setText(QCoreApplication.translate("MainWindow", u"RSTP:", None))
         self.pushButton_add.setText(QCoreApplication.translate("MainWindow", u"Add c\u00e2mera", None))
-        self.label_2.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
-        self.label.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
+        self.pushButton_del.setText(QCoreApplication.translate("MainWindow", u"Delete c\u00e2mera", None))
     # retranslateUi
 
