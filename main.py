393c70ee23f4e46b2dd50facf4cb2f3840c39ae8
eltonfernando <eltonfernando90@gmail.com>
2022-02-02 12:41:43 -0400

    config log

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 from PySide6.QtWidgets import QApplication
 from monitor.controller import Controller
+import logging
 
 if __name__ == "__main__":
+    DEBUG = True
+    if DEBUG:
+        logging.basicConfig(level=logging.DEBUG,
+                            format='%(levelname)s::'
+                                   '%(asctime)s::'
+                                   '%(filename)s::'
+                                   '%(funcName)s::'
+                                   'line %(lineno)d::'
+                                   '%(message)s')
+
+    else:
+        logging.basicConfig(level=logging.INFO,
+                            format='%(levelname)s::'
+                                   '%(asctime)s::'
+                                   '%(filename)s::'
+                                   '%(funcName)s::'
+                                   'line %(lineno)d::'
+                                   '%(message)s',
+                            filename=os.path.join("log", "loger.log"))
+
+
+
+
     app = QApplication()
 
     janela = Controller()
